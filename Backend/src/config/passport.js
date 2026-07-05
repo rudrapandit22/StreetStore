e7af8a497a08cd623a2713d44fa91477043c4d6a
rudrapandit22 <rudrashar732.8b@gmail.com>
2026-07-05 21:11:05 +0530

    Final

--- Backend/src/config/passport.js
+++ Backend/src/config/passport.js
@@ -8,7 +8,9 @@ passport.use(
         {
             clientID: config.GOOGLE_CLIENT_ID,
             clientSecret: config.GOOGLE_CLIENT_SECRET,
-            callbackURL: "https://streetstore.onrender.com/api/auth/google/callback",
+            callbackURL: process.env.NODE_ENV === "production"
+                ? "https://streetstore.onrender.com/api/auth/google/callback"
+                : "http://localhost:3000/api/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
